b222a5998928d8829fcd50b1c03d908980ce55c5
Shivam <sbarwey@polaris-login-04.hsn.cm.polaris.alcf.anl.gov>
2023-07-06 20:34:53 +0000

    SB: update to postproc

--- postprocess.py
+++ postprocess.py
@@ -239,12 +239,11 @@ if 1 == 1:
     modelpath_list.append('saved_models/pretrained_topk_unet_rollout_1_down_topk_1_1_up_topk_1_factor_4_hc_128_down_enc_2_2_2_up_enc_2_2_down_dec_2_2_2_up_dec_2_2_param_sharing_0.tar')
     modelpath_list.append('saved_models/pretrained_topk_unet_rollout_2_down_topk_1_1_up_topk_1_factor_4_hc_128_down_enc_2_2_2_up_enc_2_2_down_dec_2_2_2_up_dec_2_2_param_sharing_0.tar')
     modelpath_list.append('saved_models/pretrained_topk_unet_rollout_3_down_topk_1_1_up_topk_1_factor_4_hc_128_down_enc_2_2_2_up_enc_2_2_down_dec_2_2_2_up_dec_2_2_param_sharing_0.tar')
-
-
-
+    modelpath_list.append('saved_models/pretrained_topk_unet_rollout_4_down_topk_1_1_up_topk_1_factor_4_hc_128_down_enc_2_2_2_up_enc_2_2_down_dec_2_2_2_up_dec_2_2_param_sharing_0.tar')
+    modelpath_list.append('saved_models/pretrained_topk_unet_rollout_5_down_topk_1_1_up_topk_1_factor_4_hc_128_down_enc_2_2_2_up_enc_2_2_down_dec_2_2_2_up_dec_2_2_param_sharing_0.tar')
 
     # Load rmse data: 
-    if 1 == 1:
+    if 1 == 0:
         rmse_path = './outputs/postproc/rmse_data/'
 
         rmse_baseline = np.load(rmse_path + 'topk_unet_rollout_1_down_topk_2_up_topk_factor_4_hc_128_down_enc_2_2_2_up_enc_2_2_down_dec_2_2_2_up_dec_2_2_param_sharing_0.npy')
@@ -307,7 +306,7 @@ if 1 == 1:
 
 
     # Write data: 
-    if 1 == 0: 
+    if 1 == 1: 
         for modelpath in modelpath_list:
             p = torch.load(modelpath)
             input_dict = p['input_dict']
@@ -342,7 +341,7 @@ if 1 == 1:
             model.eval()
 
             # ~~~~ Re-load data: 
-            rollout_eval = 5 # where to evaluate the RMSE  
+            rollout_eval = 10 # where to evaluate the RMSE  
             vtk_file_test = 'datasets/BACKWARD_FACING_STEP/Backward_Facing_Step_Cropped_Re_32564.vtk'
             path_to_ei = 'datasets/BACKWARD_FACING_STEP/edge_index'
             path_to_ea = 'datasets/BACKWARD_FACING_STEP/edge_attr'
