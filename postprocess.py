d5c6d1a3e21fb80dcfb6fe8c24b5d71cf22c9c0e
Shivam Barwey <sbarwey@ShivamsMBPWork.lan>
2023-11-13 19:42:47 -0600

    SB: postproc update

--- postprocess.py
+++ postprocess.py
@@ -7,63 +7,13 @@ from torch_geometric.data import Data
 import torch_geometric.utils as utils
 import torch_geometric.nn as tgnn 
 import matplotlib.pyplot as plt
+import dataprep.nekrs_graph_setup as ngs
+import models.gnn as gnn
 
-Tensor = torch.Tensor 
-TORCH_FLOAT = torch.float32
-NP_FLOAT = np.float32
-TORCH_INT = torch.int64
-NP_INT = np.int64
-
-DIM = 3 
-SIZE = 4
-#CASE_PATH = '/Users/sbarwey/Files/nekrs_cases/examples_v23/tgv_gnn'
-CASE_PATH = '/Users/sbarwey/Files/nekrs_cases/examples_v23/tgv'
-P_HI = 7 
-P_LO = 7 
-
-def get_pygeom_dataset(data_path: str,
-                       rank: int) -> Data:
-    print('Loading data: %s, rank %s' %(data_path, rank))
-    t_load = time.time()
-    edge_index = np.loadtxt(data_path + '/edge_index_element_local_rank_%d_size_%d' %(rank, SIZE), 
-                            dtype=NP_INT).T 
-    node_element_ids = np.loadtxt(data_path + '/node_element_ids_rank_%d_size_%d' %(rank, SIZE), dtype=NP_INT)
-    global_ids = np.loadtxt(data_path + '/global_ids_rank_%d_size_%d' %(rank, SIZE), dtype=NP_INT)
-    pos = np.loadtxt(data_path + '/pos_node_rank_%d_size_%d' %(rank, SIZE), dtype=NP_FLOAT)
-    x = np.loadtxt(data_path + '/fld_u_rank_%d_size_%d' %(rank,SIZE), dtype=NP_FLOAT)
-
-    # Make tensor 
-    edge_index = torch.tensor(edge_index)
-    node_element_ids = torch.tensor(node_element_ids)
-    global_ids = torch.tensor(global_ids)
-    pos = torch.tensor(pos)
-    x = torch.tensor(x)
-
-    # # un-batch elements 
-    # utils.unbatch(pos, node_element_ids, dim=0)
-    # utils.unbatch(x, node_element_ids, dim=0)
-
-    # Make data 
-    data = Data(x=x, 
-                pos=pos, 
-                edge_index=edge_index, 
-                node_element_ids=node_element_ids, 
-                global_ids=global_ids,
-                batch=node_element_ids)
-    t_load = time.time() - t_load 
-    print('\ttook %g sec' %(t_load))
-    return data 
-
-def get_data_statistics(data_path_list):
-    return 
-
-
-def get_rms(x_batch: Tensor) -> Tensor:
-    u_var = x_batch.var(dim=1, keepdim=True)
-    tke = 0.5*u_var.sum(dim=2)
-    u_rms = torch.sqrt(tke / 1.5)
-    return u_rms 
-
+seed = 122
+torch.manual_seed(seed)
+np.random.seed(seed)
+torch.set_grad_enabled(False)
 
 def ratio_boundary_internal_nodes(p: int) -> float: 
     ratio = ( (p+1)**3 - (p-1)**3 )/(p-1)**3
@@ -71,63 +21,154 @@ def ratio_boundary_internal_nodes(p: int) -> float:
 
 if __name__ == "__main__":
 
-    rank = 0
-    data_path_hi = CASE_PATH + '/gnn_outputs_original_poly_%d' %(P_HI)
-    data_hi = get_pygeom_dataset(data_path_hi, rank)
-    data_path_lo = CASE_PATH + '/gnn_outputs_recon_poly_%d' %(P_LO)
-    data_lo = get_pygeom_dataset(data_path_lo, rank)
-   
-    # Split into multiple graphs based on element ids  
-    pos_hi = torch.stack(utils.unbatch(data_hi.pos, data_hi.node_element_ids, dim=0))
-    x_hi = torch.stack(utils.unbatch(data_hi.x, data_hi.node_element_ids, dim=0))
-    gli_hi = torch.stack(utils.unbatch(data_hi.global_ids, data_hi.node_element_ids, dim=0))
+    case_path = './datasets/tgv'
+    device_for_loading = 'cpu'
+    fraction_valid = 0.1 
 
-    pos_lo = torch.stack(utils.unbatch(data_lo.pos, data_lo.node_element_ids, dim=0))
-    x_lo = torch.stack(utils.unbatch(data_lo.x, data_lo.node_element_ids, dim=0))
-    gli_lo = torch.stack(utils.unbatch(data_lo.global_ids, data_lo.node_element_ids, dim=0))
+    train_dataset = []
+    test_dataset = []  
 
+    data_read_world_size = 4 
 
-    # ~~~~ Postprocessing the data  
+    for i in range(data_read_world_size):
+        if i != 3:
+            continue
+        data_x_path = case_path + '/gnn_outputs_original_poly_7' + '/fld_u_rank_%d_size_%d' %(i,data_read_world_size) # input  
+        data_y_path = case_path + '/gnn_outputs_recon_poly_7' + '/fld_u_rank_%d_size_%d' %(i,data_read_world_size) # target 
+        edge_index_path = case_path + '/gnn_outputs_original_poly_7' + '/edge_index_element_local_rank_%d_size_%d' %(i,data_read_world_size) 
+        node_element_ids_path = case_path + '/gnn_outputs_original_poly_7' + '/node_element_ids_rank_%d_size_%d' %(i,data_read_world_size)
+        global_ids_path = case_path + '/gnn_outputs_original_poly_7' + '/global_ids_rank_%d_size_%d' %(i,data_read_world_size) 
+        pos_path = case_path + '/gnn_outputs_original_poly_7' + '/pos_node_rank_%d_size_%d' %(i,data_read_world_size) 
+
+        # note: data_mean and data_std are overwritten. the one used is for i = 3 
+        train_dataset_temp, test_dataset_temp, data_mean, data_std = ngs.get_pygeom_dataset(
+                                                             data_x_path, 
+                                                             data_y_path,
+                                                             edge_index_path,
+                                                             node_element_ids_path,
+                                                             global_ids_path,
+                                                             pos_path,
+                                                             device_for_loading,
+                                                             fraction_valid)
+
+        train_dataset += train_dataset_temp
+        test_dataset += test_dataset_temp
+
+    # ~~~~ postprocessing: training losses 
     if 1 == 0:
-        # get rms 
-        rms_hi = get_rms(x_hi)
-        rms_lo = get_rms(x_lo)
+        mp = [1,2,3,4,5,6,7,8]
 
-        # get prediction errors 
-        element_error = (x_hi - x_lo)**2
-        element_error = element_error.mean(dim=[1,2])
-
-        # contribution of boundary nodes to the error 
-        mask_internal = gli_lo == 0
-        mask_boundary = ~mask_internal
-         
-        mask_internal = mask_internal.unsqueeze(-1)
-        mask_boundary = mask_boundary.unsqueeze(-1)
-
-        element_error_boundary =  ((mask_boundary*x_hi - mask_boundary*x_lo)**2).mean(dim=[1,2]) 
-        element_error_internal =  ((mask_internal*x_hi - mask_internal*x_lo)**2).mean(dim=[1,2]) 
-
-
-    # ~~~~ postprocessing the gnn model  
-    if 1 == 0:
-        a = torch.load('./saved_models/super_res_gnn.tar')
-        b = torch.load('./saved_models/super_res_gnn_b32.tar')
+        conv_loss_train = []
+        conv_loss_valid = []
+        plt.rcParams.update({'font.size': 18})
         fig, ax = plt.subplots()
-        ax.plot(a['loss_hist_train'])
-        ax.plot(b['loss_hist_train'])
+        for i in mp:
+            a = torch.load('./saved_models/super_res_gnn_mp_%d.tar' %(i))
+            ax.plot(a['loss_hist_train'], label='mp %d' %(i))
+            conv_loss_train.append(np.mean(a['loss_hist_train'][-10:]))
+            conv_loss_valid.append(np.mean(a['loss_hist_test'][-10:]))
         ax.set_yscale('log')
+        ax.legend()
         plt.show(block=False)
      
+        ms=15
+        mew=2
+        lw=1
+        fig, ax = plt.subplots()
+        ax.plot(mp, conv_loss_train, marker='o', fillstyle='none', lw=lw, ms=ms, mew=mew, color='black')
+        ax.plot(mp, conv_loss_valid, marker='s', fillstyle='none', lw=lw, ms=ms, mew=mew, color='blue')
+        ax.set_ylabel('Super-Resolution Loss')
+        ax.set_xlabel('Message Passing Layers')
+        ax.set_yscale('log')
+        plt.show(block=False)
 
-    # ~~~~ Plotting 
-    if 1 == 0:
+
+    # ~~~~ Visualize model predictions 
+    if 1 == 1:
+        mp = [1,2,3,4,5,6,7,8] 
+        x_list = [] 
+        y_pred_list = []
+        y_target_list = []
+
+        for i in mp:
+            a = torch.load('./saved_models/super_res_gnn_mp_%d.tar' %(i))
+            sample = train_dataset[0]
+            n_messagePassing_layers = i 
+            
+            input_dict = a['input_dict']
+            input_channels = input_dict['input_channels']
+            hidden_channels = input_dict['hidden_channels']
+            output_channels = input_dict['output_channels']
+            n_mlp_layers = input_dict['n_mlp_layers']
+            activation = input_dict['activation']
+            name = 'temp'
+
+            model = gnn.mp_gnn(input_channels,
+                           hidden_channels,
+                           output_channels,
+                           n_mlp_layers,
+                           n_messagePassing_layers,
+                           activation,
+                           name)
+            
+            model.load_state_dict(a['state_dict'])
+            device = 'cpu'
+            model.to(device)
+            model.eval()
+
+            with torch.no_grad():
+
+                #element 1: 100 
+                #element 2: 1000
+                #element 3: 23 
+                #element 4: 1100
+                data = test_dataset[1100]
+
+                # scale input 
+                x_in = (data.x - data_mean[0])/data_std[0]
+
+                # evaluate 
+                y_pred_scaled = model(x_in, data.edge_index, data.pos_norm, data.batch)
+
+                # unscale output 
+                y_pred = y_pred_scaled * data_std[1] + data_mean[1] 
+                y_target = data.y
+                x = data.x
+
+                y_pred_list.append(y_pred)
+                y_target_list.append(y_target)
+                x_list.append(x)
+
+
+        plt.rcParams.update({'font.size': 14})
+        for comp in range(3):
+            fig, ax = plt.subplots(2,4, figsize=(15,7))
+            count = 0 
+            for r in range(2):
+                for c in range(4):
+                    i = mp[count]
+                    x = x_list[count]
+                    y_target = y_target_list[count]
+                    y_pred = y_pred_list[count]
+                    ax[r,c].scatter(x[:,comp], y_target[:,comp], color='red')
+                    ax[r,c].scatter(y_pred[:,comp], y_target[:,comp], color='lime')
+                    ax[r,c].plot([y_target[:,comp].min(), y_target[:,comp].max()],
+                               [y_target[:,comp].min(), y_target[:,comp].max()],
+                               color='black', lw=2)
+                    ax[r,c].set_title('n_mp=%d' %(i))
+                    ax[r,c].set_xlabel('Prediction')
+                    ax[r,c].set_ylabel('Target')
+                    count += 1 
+            plt.show(block=False)
+
+    # ~~~~ Plotting  
+    if 1 == 1:
         print('Plotting...')
 
         # Plot the edges 
-        if 1 == 0:
-            
+        if 1 == 1:
             #for rl in range(SIZE):
-            data = Data(pos = pos_hi[0], x = pos_hi[0], edge_index = data_hi.edge_index) 
+            data = train_dataset[0]
             G = utils.to_networkx(data=data)
 
             
@@ -245,7 +286,20 @@ if __name__ == "__main__":
 
         # Error budget: boundary vs internal 
         if 1 == 0:
-        
+            # get prediction errors 
+            element_error = (x_hi - x_lo)**2
+            element_error = element_error.mean(dim=[1,2])
+
+            # contribution of boundary nodes to the error 
+            mask_internal = gli_lo == 0
+            mask_boundary = ~mask_internal
+             
+            mask_internal = mask_internal.unsqueeze(-1)
+            mask_boundary = mask_boundary.unsqueeze(-1)
+
+            element_error_boundary =  ((mask_boundary*x_hi - mask_boundary*x_lo)**2).mean(dim=[1,2]) 
+            element_error_internal =  ((mask_internal*x_hi - mask_internal*x_lo)**2).mean(dim=[1,2]) 
+           
             # sort elements by error 
             error_total, idx_sort = torch.sort(element_error, descending=True)
 
