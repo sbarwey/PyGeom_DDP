6bf8f3f620a5ce607b4670853c220548b7e5b344
Shivam Barwey <sbarwey@ShivamsMBPWork.lan>
2024-01-17 15:34:33 -0600

    SB: postproc update

--- postprocess.py
+++ postprocess.py
@@ -21,54 +21,41 @@ def ratio_boundary_internal_nodes(p: int) -> float:
 
 if __name__ == "__main__":
 
-    case_path = './datasets/tgv'
-    device_for_loading = 'cpu'
-    fraction_valid = 0.1 
+    
+    # Load a dataset 
+    data_dir = "/Volumes/Novus_SB_14TB/ml/DDP_PyGeom_SR/datasets/"
+    train_dataset = torch.load(data_dir + "Single_Snapshot_Re_1600_T_9.0/train_dataset.pt")
+    test_dataset = torch.load(data_dir + "Single_Snapshot_Re_1600_T_9.0/valid_dataset.pt")
+    data_mean = torch.load(data_dir + "Single_Snapshot_Re_1600_T_9.0/data_mean.pt")
+    data_std = torch.load(data_dir + "Single_Snapshot_Re_1600_T_9.0/data_std.pt")
 
-    train_dataset = []
-    test_dataset = []  
+    # convert statistics to float32 : torch.float32 --- tensor = tensor.to(torch.float32) 
+    data_mean[0] = data_mean[0].to(torch.float32)
+    data_mean[1] = data_mean[1].to(torch.float32)
+    data_std[0] = data_std[0].to(torch.float32)
+    data_std[1] = data_std[1].to(torch.float32)
 
-    data_read_world_size = 4 
-
-    for i in range(data_read_world_size):
-        if i != 3:
-            continue
-        data_x_path = case_path + '/gnn_outputs_original_poly_7' + '/fld_u_rank_%d_size_%d' %(i,data_read_world_size) # input  
-        data_y_path = case_path + '/gnn_outputs_recon_poly_7' + '/fld_u_rank_%d_size_%d' %(i,data_read_world_size) # target 
-        edge_index_path = case_path + '/gnn_outputs_original_poly_7' + '/edge_index_element_local_rank_%d_size_%d' %(i,data_read_world_size) 
-        node_element_ids_path = case_path + '/gnn_outputs_original_poly_7' + '/node_element_ids_rank_%d_size_%d' %(i,data_read_world_size)
-        global_ids_path = case_path + '/gnn_outputs_original_poly_7' + '/global_ids_rank_%d_size_%d' %(i,data_read_world_size) 
-        pos_path = case_path + '/gnn_outputs_original_poly_7' + '/pos_node_rank_%d_size_%d' %(i,data_read_world_size) 
-
-        # note: data_mean and data_std are overwritten. the one used is for i = 3 
-        train_dataset_temp, test_dataset_temp, data_mean, data_std = ngs.get_pygeom_dataset(
-                                                             data_x_path, 
-                                                             data_y_path,
-                                                             edge_index_path,
-                                                             node_element_ids_path,
-                                                             global_ids_path,
-                                                             pos_path,
-                                                             device_for_loading,
-                                                             fraction_valid)
-
-        train_dataset += train_dataset_temp
-        test_dataset += test_dataset_temp
 
     # ~~~~ postprocessing: training losses 
-    if 1 == 0:
+    if 1 == 1:
         mp = [1,2,3,4,5,6,7,8]
+        mp = [5]
 
         conv_loss_train = []
         conv_loss_valid = []
+
         plt.rcParams.update({'font.size': 18})
         fig, ax = plt.subplots()
         for i in mp:
-            a = torch.load('./saved_models/super_res_gnn_mp_%d.tar' %(i))
-            ax.plot(a['loss_hist_train'], label='mp %d' %(i))
+            a = torch.load('./saved_models/single_snapshot_t_9/batch_size_8/super_res_gnn_mp_%d.tar' %(i))
+            a = torch.load('./saved_models/identity_map_test/super_res_gnn_mp_%d.tar' %(i))
+            ax.plot(a['loss_hist_train'], label='mp %d' %(i), lw=2)
             conv_loss_train.append(np.mean(a['loss_hist_train'][-10:]))
             conv_loss_valid.append(np.mean(a['loss_hist_test'][-10:]))
         ax.set_yscale('log')
         ax.legend()
+        ax.set_xlabel('Epochs')
+        ax.set_ylabel('Loss')
         plt.show(block=False)
      
         ms=15
@@ -79,20 +66,124 @@ if __name__ == "__main__":
         ax.plot(mp, conv_loss_valid, marker='s', fillstyle='none', lw=lw, ms=ms, mew=mew, color='blue')
         ax.set_ylabel('Super-Resolution Loss')
         ax.set_xlabel('Message Passing Layers')
+        ax.set_ylim([0.05, 0.07])
+        #ax.set_yscale('log')
+        plt.show(block=False)
+
+    # ~~~~ Analyze predictions -- all elements 
+    if 1 == 0:
+        mp = 6 
+
+        a = torch.load('./saved_models/single_snapshot_t_9/batch_size_8/super_res_gnn_mp_%d.tar' %(mp))
+        n_messagePassing_layers = mp 
+        
+        input_dict = a['input_dict']
+        input_channels = input_dict['input_channels']
+        hidden_channels = input_dict['hidden_channels']
+        output_channels = input_dict['output_channels']
+        n_mlp_layers = input_dict['n_mlp_layers']
+        activation = input_dict['activation']
+        name = 'temp'
+
+        model = gnn.mp_gnn(input_channels,
+                       hidden_channels,
+                       output_channels,
+                       n_mlp_layers,
+                       n_messagePassing_layers,
+                       activation,
+                       name)
+        
+        model.load_state_dict(a['state_dict'])
+        device = 'cpu'
+        model.to(device)
+        model.eval()
+
+        with torch.no_grad():
+            x_input_list = [] 
+            y_pred_list = []
+            y_target_list = []
+
+
+            dataset = train_dataset 
+            N = len(dataset) 
+            mse_before_gnn = torch.zeros(N)
+            mse_after_gnn = torch.zeros(N) 
+            rms_input = torch.zeros(N) 
+            rms_target = torch.zeros(N)
+
+            for i in range(N):
+                print('evaluating %d/%d' %(i+1, N))
+                data = dataset[i]
+
+                # scale input 
+                x_in = (data.x - data_mean[0])/data_std[0]
+
+                # evaluate 
+                y_pred_scaled = model(x_in, data.edge_index, data.pos_norm, data.batch)
+
+                # unscale output 
+                y_pred = y_pred_scaled * data_std[1] + data_mean[1] 
+                y_target = data.y
+                x_input = data.x[:, :3]
+
+                # y_pred_list.append(y_pred)
+                # y_target_list.append(y_target)
+                # x_input_list.append(x_input)
+
+                # loss before gnn 
+                mse_before_gnn[i] = F.mse_loss(x_input, y_target)
+
+                # loss after gnn 
+                mse_after_gnn[i] = F.mse_loss(y_pred, y_target) 
+
+                # rms
+                rms_input[i] = data.x_rms
+                rms_target[i] = data.y_rms
+
+        # Visualize
+        element_ids = torch.arange(N) 
+    
+        # Get the difference in MSE 
+        # if negative, bad
+        # if positive, good 
+        difference = mse_before_gnn - mse_after_gnn
+
+        # sort the difference  
+        idx_sort = torch.sort(difference, descending=True)[1]
+
+        element_ids_sorted = element_ids[idx_sort]
+        difference_sorted = difference[idx_sort]
+
+        plt.rcParams.update({'font.size': 14})
+
+        # Plotting error difference versus element 
+        fig, ax = plt.subplots()
+        #ax.plot(element_ids, difference_sorted, lw=2)
+        ax.plot(element_ids, difference_sorted.abs(), lw=2)
+        ax.set_xlabel('Element IDs (Sorted)')
+        ax.set_ylabel('Model Gain')
         ax.set_yscale('log')
         plt.show(block=False)
 
+        # Plotting error difference versus RMS 
+        fig, ax = plt.subplots()
+        ax.scatter(difference, rms_target)
+        ax.set_xlabel('Model Gain')
+        ax.set_ylabel('Target RMS Velocity')
+        plt.show(block=False)
 
-    # ~~~~ Visualize model predictions 
+
+    # ~~~~ Visualize model predictions -- a single element  
     if 1 == 0:
+        element_ids_sorted = torch.load("/Users/sbarwey/Files/ml/DDP_PyGeom_SR/outputs/postproc/element_ids_sorted.pt")
         mp = [1,2,3,4,5,6,7,8] 
         x_list = [] 
         y_pred_list = []
         y_target_list = []
 
         for i in mp:
-            a = torch.load('./saved_models/super_res_gnn_mp_%d.tar' %(i))
-            sample = train_dataset[0]
+            #a = torch.load('./saved_models/super_res_gnn_mp_%d.tar' %(i))
+            a = torch.load('./saved_models/single_snapshot_t_9/batch_size_8/super_res_gnn_mp_%d.tar' %(i))
             n_messagePassing_layers = i 
             
             input_dict = a['input_dict']
@@ -122,7 +213,8 @@ if __name__ == "__main__":
                 #element 2: 1000
                 #element 3: 23 
                 #element 4: 1100
-                data = test_dataset[1100]
+                data = train_dataset[0]
+                data = test_dataset[element_ids_sorted[-1]]
 
                 # scale input 
                 x_in = (data.x - data_mean[0])/data_std[0]
@@ -163,7 +255,7 @@ if __name__ == "__main__":
 
 
     # ~~~~ Postprocess run logs: KE, dissipation, enst
-    if 1 == 1:
+    if 1 == 0:
         import re
 
         def read_nrs_log(file_path):
@@ -212,14 +304,12 @@ if __name__ == "__main__":
         ax.set_ylabel('-dEk/dt')
         plt.show(block=False)
 
-
-
     # ~~~~ Plotting  
     if 1 == 0:
         print('Plotting...')
 
         # Plot the edges 
-        if 1 == 1:
+        if 1 == 0:
             #for rl in range(SIZE):
             data = train_dataset[0]
             G = utils.to_networkx(data=data)
@@ -254,75 +344,45 @@ if __name__ == "__main__":
             ax.set_aspect('equal')
             fig.tight_layout()
             plt.show(block=False)
-
-        # Plot fine and coarse element positions, for one element   
-        if 1 == 0:
-            e_id = 510
-            ms = 50
-            fig = plt.figure()
-            ax = fig.add_subplot(111, projection='3d')
-            ax.scatter(pos_hi[e_id][:,0], pos_hi[e_id][:,1], pos_hi[e_id][:,2], c='black', marker='o', s=ms)
-            ax.scatter(pos_lo[e_id][:,0], pos_lo[e_id][:,1], pos_lo[e_id][:,2], c='blue', marker='o', s=ms)
-            ax.set_xlabel('X Label')
-            ax.set_ylabel('Y Label')
-            ax.set_zlabel('Z Label')
-            plt.show(block=False)
-
-        # Isolate internal nodes from boundary nodes 
-        if 1 == 0: 
-            e_id = 325
-            mask_internal = gli_lo[e_id] == 0
-            mask_boundary = ~mask_internal 
-            ms = 50
-            fig = plt.figure()
-            ax = fig.add_subplot(111, projection='3d')
-            ax.scatter(pos_hi[e_id][mask_internal,0], 
-                       pos_hi[e_id][mask_internal,1], 
-                       pos_hi[e_id][mask_internal,2], c='blue', marker='o', s=ms)
-            ax.scatter(pos_hi[e_id][mask_boundary,0], 
-                       pos_hi[e_id][mask_boundary,1], 
-                       pos_hi[e_id][mask_boundary,2], c='red', marker='o', s=ms)
-            ax.set_xlabel('X Label')
-            ax.set_ylabel('Y Label')
-            ax.set_zlabel('Z Label')
-            plt.show(block=False)
     
         # Plot scatter plots of node features 
         if 1 == 0:
-            e_id = 45
+            e_id = 0
+
+            sample = train_dataset[e_id]
 
             color='red' 
             fig, ax = plt.subplots(1,3, figsize=(10,4))
-            ax[0].scatter(x_lo[e_id][:,0], x_hi[e_id][:,0], c=color)
-            ax[0].plot([x_hi[e_id][:,0].min(), x_hi[e_id][:,0].max()], 
-                       [x_hi[e_id][:,0].min(), x_hi[e_id][:,0].max()],
-                       color='black', lw=2)
-
-            ax[1].scatter(x_lo[e_id][:,1], x_hi[e_id][:,1], c=color)
-            ax[1].plot([x_hi[e_id][:,1].min(), x_hi[e_id][:,1].max()], 
-                       [x_hi[e_id][:,1].min(), x_hi[e_id][:,1].max()],
-                       color='black', lw=2)
-
-            ax[2].scatter(x_lo[e_id][:,2], x_hi[e_id][:,2], c=color)
-            ax[2].plot([x_hi[e_id][:,2].min(), x_hi[e_id][:,2].max()], 
-                       [x_hi[e_id][:,2].min(), x_hi[e_id][:,2].max()],
-                       color='black', lw=2)
             
-            ax[0].set_ylabel('u_f')
-            ax[0].set_xlabel('P u_c')
+            for comp in range(3):
+                ax[comp].scatter(sample.x[:,comp], sample.y[:,comp], c=color)
+                ax[comp].plot( [sample.y[:,comp].min(), sample.y[:,comp].max()], 
+                               [sample.y[:,comp].min(), sample.y[:,comp].max()],
+                           color='black', lw=2)
+
+            ax[0].set_xlabel('x')
+            ax[0].set_ylabel('y')
             ax[0].set_title('e_id = %d' %(e_id))
             ax[0].set_aspect('equal')
 
-            ax[1].set_ylabel('u_f')
-            ax[1].set_xlabel('P u_c')
+            ax[1].set_xlabel('x')
+            ax[1].set_ylabel('y')
             ax[1].set_title('e_id = %d' %(e_id))
             ax[1].set_aspect('equal')
 
-            ax[2].set_ylabel('u_f')
-            ax[2].set_xlabel('P u_c')
+            ax[2].set_xlabel('x')
+            ax[2].set_ylabel('y')
             ax[2].set_title('e_id = %d' %(e_id))
             ax[2].set_aspect('equal')
             plt.show(block=False)
+
+
+        # Global scatter plots of training data -- joint distributions 
+        if 1 == 1:
+            
+
+
+            pass 
 
         # RMS versus reconstruction MSE 
         if 1 == 0:
@@ -335,48 +395,4 @@ if __name__ == "__main__":
             ax.set_ylabel('Reconstruction MSE', fontsize=20)
             ax.legend(fontsize=20)
             plt.show(block=False)
-
-
-        # Error budget: boundary vs internal 
-        if 1 == 0:
-            # get prediction errors 
-            element_error = (x_hi - x_lo)**2
-            element_error = element_error.mean(dim=[1,2])
-
-            # contribution of boundary nodes to the error 
-            mask_internal = gli_lo == 0
-            mask_boundary = ~mask_internal
-             
-            mask_internal = mask_internal.unsqueeze(-1)
-            mask_boundary = mask_boundary.unsqueeze(-1)
-
-            element_error_boundary =  ((mask_boundary*x_hi - mask_boundary*x_lo)**2).mean(dim=[1,2]) 
-            element_error_internal =  ((mask_internal*x_hi - mask_internal*x_lo)**2).mean(dim=[1,2]) 
-           
-            # sort elements by error 
-            error_total, idx_sort = torch.sort(element_error, descending=True)
-
-            error_boundary = element_error_boundary[idx_sort]
-            error_internal = element_error_internal[idx_sort]
-
-            lw=1
-            fig, ax = plt.subplots(1,2, figsize=(10,4))
-            ax[0].plot(error_total, color='black', lw=lw, label='Total Error')
-            ax[0].plot(error_boundary, color='red', lw=lw, label='From Boundary Nodes')
-            ax[0].plot(error_internal, color='blue', lw=lw, label='From Internal Nodes')
-            ax[0].set_xlabel('Element ID')
-            ax[0].set_ylabel('Reconstruction Error')
-
-            ax[1].plot((error_boundary/error_total)*100, color='red', lw=lw, label='From Boundary Nodes')
-            ax[1].plot((error_internal/error_total)*100, color='blue', lw=lw, label='From Internal Nodes')
-            ax[1].set_xlabel('Element ID')
-            ax[1].set_ylabel('Error Contribution [%]')
-
-            #ax.set_yscale('log')
-            #ax.set_xscale('log')
-            ax[0].legend(fancybox=False, prop={'size': 14})
-            plt.show(block=False)
-        
-
-
 
